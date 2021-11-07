c4d1f0e06a9731e8ec7ec6013b1e8eede36755f7
azkahariz <57719437+azkahariz@users.noreply.github.com>
2021-11-07 14:19:15 +0700

    Update versi 1.2
    Menambahkan prediction interval dan memperbaiki cara memplotting

--- main.m
+++ main.m
@@ -2,10 +2,9 @@
 % Title        : Simple Linear Regression
 % Author       : Azka Hariz
 % Date         : November 7, 2021
-% Code version : 1.1
+% Code version : 1.2
 % Availability : https://github.com/azkahariz/SimpleLinearRegression
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
 clear all, close all, clc
 
 %% Read data
@@ -41,19 +40,25 @@ F0  = MSR/MSE;             % Uji F-dist. hipotesis H0: B1 = 0;
 t0  = Beta1/sqrt(var/Sxx); % Uji t-dist. hipotesis H0: B1 = 0;
 
 %% Confidence Intervals 95%
+x0_inp = input('Masukan nilai x0 untuk mencari confidence interval dan prediction interval : ');
+x0 = min(Data.X):0.01:max(Data.X)';
 % Confidence intervals on parameters
 B1_min = Beta1 - 2.101*sqrt(var/Sxx);                   % B1 minimum
 B1_max = Beta1 + 2.101*sqrt(var/Sxx);                   % B1 maksimum
 B0_min = Beta0 - 2.101*sqrt(var*((1/n) + avg_x^2/Sxx)); % B0 minimum
 B0_max = Beta0 + 2.101*sqrt(var*((1/n) + avg_x^2/Sxx)); % B0 minimum
 % Confidence intervals on the mean response
-x0_inp = input('Masukan nilai x0 untuk mencari confidence interval dari mean response : ');
-y_min_x0  = Beta0 + Beta1*x0_inp - 2.101*sqrt(var*( (1/n) + (x0_inp-avg_x).^2/Sxx ));
-y_max_x0  = Beta0 + Beta1*x0_inp + 2.101*sqrt(var*( (1/n) + (x0_inp-avg_x).^2/Sxx ));
+y_min_x0_conf  = Beta0 + Beta1*x0_inp - 2.101*sqrt(var*( (1/n) + (x0_inp-avg_x).^2/Sxx ));
+y_max_x0_conf  = Beta0 + Beta1*x0_inp + 2.101*sqrt(var*( (1/n) + (x0_inp-avg_x).^2/Sxx ));
+y_min_conf     = Beta0 + Beta1*x0 - 2.101*sqrt(var*( (1/n) + (x0-avg_x).^2/Sxx ));
+y_max_conf     = Beta0 + Beta1*x0 + 2.101*sqrt(var*( (1/n) + (x0-avg_x).^2/Sxx ));
 
-x0 = min(Data.X):0.01:max(Data.X)';
-y_min  = Beta0 + Beta1*x0 - 2.101*sqrt(var*( (1/n) + (x0-avg_x).^2/Sxx ));
-y_max  = Beta0 + Beta1*x0 + 2.101*sqrt(var*( (1/n) + (x0-avg_x).^2/Sxx ));
+%% Prediction interval of new observations
+% Confidence intervals on the mean response
+y_min_x0_pred  = Beta0 + Beta1*x0_inp - 2.101*sqrt(var*( 1 + (1/n) + (x0_inp-avg_x).^2/Sxx ));
+y_max_x0_pred  = Beta0 + Beta1*x0_inp + 2.101*sqrt(var*( 1 + (1/n) + (x0_inp-avg_x).^2/Sxx ));
+y_min_pred     = Beta0 + Beta1*x0 - 2.101*sqrt(var*( 1 + (1/n) + (x0-avg_x).^2/Sxx ));
+y_max_pred     = Beta0 + Beta1*x0 + 2.101*sqrt(var*( 1 + (1/n) + (x0-avg_x).^2/Sxx ));
 
 %% Show output in command windows
 fprintf('Persamaan linear reggression:\n');
@@ -72,18 +77,27 @@ fprintf('%.3f < B1 < %.3f\n', B1_min, B1_max);
 fprintf('%.3f < B0 < %.3f\n\n', B0_min, B0_max);
 
 fprintf('Confidence intervals 95%% on the mean response x = %.3f:\n',x0_inp);
-fprintf('%.3f < y < %.3f\n', y_min_x0, y_max_x0);
+fprintf('%.3f < y < %.3f\n\n', y_min_x0_conf, y_max_x0_conf);
+
+fprintf('Prediction intervals 95%% on the mean response x = %.3f:\n',x0_inp);
+fprintf('%.3f < y < %.3f\n', y_min_x0_pred, y_max_x0_pred);
 
 %% Plotting
 figure(1)
-plot(Data.X,Data.Y,'*');
+p(1) = plot(Data.X,Data.Y,'*');
+hold on
+p(2) = plot(x,y_approx,'r');
+hold on
+p(3) = plot(x0,y_min_conf,'--g');
+hold on
+p(4) = plot(x0,y_max_conf,'--g');
+hold on
+p(5) = plot(x0,y_min_pred,'--c');
+hold on
+p(6) = plot(x0,y_max_pred,'--c');
 grid on
-hold on
-plot(x,y_approx,'r');
-hold on
-plot(x0,y_min,'--g');
-hold on
-plot(x0,y_max,'--g');
-xlabel('Hydrocarbon level (%),x');  % Kasih nama label untuk sumbu x
-ylabel('Oxygen purity (%), y');     % Kasih nama label untuk sumbu y
-legend('$y_{real}$', '$\hat{\mu}_{Y}$', '$\hat{\mu}_{Y|x_{0}} \pm t_{\alpha/2,n-2}\sqrt{V(\mu_{Y|x_{0}})}$', 'Interpreter', 'Latex','FontSize',12);
+
+% label
+xlabel('Hydrocarbon level (\%), $x$', 'Interpreter', 'Latex', 'FontSize', 12);  % Kasih nama label untuk sumbu x
+ylabel('Oxygen purity (\%), $y$', 'Interpreter', 'Latex', 'FontSize', 12);      % Kasih nama label untuk sumbu y
+legend(p([1 2 3 5]),'$y_{real}$','Regression Line ($\hat{\mu}_{Y}$)', 'Confidence Interval', 'Prediction Interval', 'Interpreter', 'Latex', 'FontSize',10);
